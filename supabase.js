62de778e0ca54f15cc249624c8cc65a663bb3d3a
mdqclio <mdqclio@gmail.com>
2026-04-21 22:16:14 -0300

    Change Supabase key in supabase.js
    Updated Supabase key to a new value.

--- supabase.js
+++ supabase.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 const SUPABASE_URL = 'https://unlhcuanfrtpatoipwve.Supabase.co';
-const SUPABASE_KEY = 'sb_publishable_gypetSX16kGMXHhG_xqLWA_7wrzWgAK';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVubGhjdWFuZnJ0cGF0b2lwd3ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3MjQ0OTcsImV4cCI6MjA5MjMwMDQ5N30.rKb8BI7fBQcRdyyyxVfBOZbtCmGYKIMLUDLVmkn1SYM';
 const CLUB_ID      = 'a6da7e40-1515-45dc-8933-4eef33ce937a';
 
 const { createClient } = supabase;
